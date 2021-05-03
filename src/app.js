a05daa85b588394df0bacb44da7f4885fcec7f67
Sarita-ok <sarakhan@windowslive.com>
2021-05-03 23:53:34 +0100

    update js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 function displayTemperature(response) {
+console.log(response.data);
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
+let windElement = document.querySelector("#wind");
 temperatureElement.innerHTML = Math.round
 (response.data.main.temp);
-cityElement.innerHTML = response.data.weather[0].description.;
+cityElement.innerHTML = response.data.name;
+descriptionElement.innerHTML = response.data.weather[0].description;
+humidistyElement.innerHTML = response.data.main.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
 }
 
 
