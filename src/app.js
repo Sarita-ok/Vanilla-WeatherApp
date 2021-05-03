4c278fe80c9917ef8918f4022ebec43abec89569
Sarita-ok <sarakhan@windowslive.com>
2021-05-03 23:13:04 +0100

    update js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
+function displayTemperature(response) {
+console.log(response.data.main.temp);
+}
+
+
 let apiKey = "b0a489d5d96ea5d4bb0300b20321b007";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=
 ${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
